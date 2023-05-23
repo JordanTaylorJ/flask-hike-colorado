b1fd1c5d1ba439545e73f0fc5a7f062c28bfa736
JordanJoseph <jordant.joseph@outlook.com>
2023-05-23 10:36:35 -0600

    create basic file structure with boilerplate code

--- server/app.py
+++ server/app.py
@@ -1,1 +1,11 @@
-from flask import Flask 
+from flask import request
+from flask_restful import Resource
+
+# Local imports
+from config import app, db, api
+#from models import 
+
+# Views go here!
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
